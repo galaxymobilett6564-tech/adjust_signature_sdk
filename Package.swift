da60929293a0c46624cd4c944643a76489b3831e
dimim <marios@adjust.com>
2024-12-11 11:52:04 +0100

    [spm] Add support for 3.35.2

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .binaryTarget(
             name: "AdjustSignature",
             // swiftlint:disable line_length
-            url: "https://github.com/adjust/adjust_signature_sdk/releases/download/v3.35.1/AdjustSigSdk-iOS-tvOS-Dynamic-3.35.1.xcframework.zip",
+            url: "https://github.com/adjust/adjust_signature_sdk/releases/download/v3.35.2/AdjustSigSdk-iOS-tvOS-Dynamic-3.35.2.xcframework.zip",
             // swiftlint:enable line_length
-            checksum: "0c43332f6988d5e777ef765fe2982719bd3c55b8b1b978303b6fbb8bfffc65a7"
+            checksum: "b10f412d7410af3947e1ae4dfe494125b0219f8ee7e1506768270bd2b8626cae"
         )
     ]
 )
