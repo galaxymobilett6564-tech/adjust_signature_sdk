055c4f692741feb99b496b26cdfc1600c84f0826
Aleksei Ilin <aleksei.ilin@adjust.com>
2025-10-06 11:44:10 +0200

    [spm] Update

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .binaryTarget(
             name: "AdjustSignature",
             // swiftlint:disable line_length
-            url: "https://github.com/adjust/adjust_signature_sdk/releases/download/v3.47.0/AdjustSigSdk-iOS-tvOS-Dynamic-3.47.0.xcframework.zip",
+            url: "https://github.com/adjust/adjust_signature_sdk/releases/download/v3.61.0/AdjustSigSdk-iOS-tvOS-Dynamic-3.61.0.xcframework.zip",
             // swiftlint:enable line_length
-            checksum: "45f8b39df8654a820a3c1478219dee685b9611d40b9f08e930276370992d4e17"
+            checksum: "74dcdada65bc60a1795f3710dcb457ceb25ef88da79c64e5ab6f3c631a5118f6"
         )
     ]
 )
